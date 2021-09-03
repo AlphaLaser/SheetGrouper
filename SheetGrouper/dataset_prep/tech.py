47e006d1d0d2e0a2c3a0d0ff792ba63116923bf1
AlphaLaser <59497243+AlphaLaser@users.noreply.github.com>
2021-09-03 15:55:37 +0530

    Add files via upload

---
+++ SheetGrouper/dataset_prep/tech.py
@@ -0,0 +1,21 @@
+tech = [
+    'Coding',
+    'Programming',
+    'Software',
+    'AI',
+    'Artificial Intelligence',
+    'ML',
+    'Machine Learning',
+    'Web Development',
+    'App Development',
+    'Video Editing',
+    'Audio Editing',
+    'Cybersecurity',
+    'Hacking',
+    'Gaming',
+    'Graphic Design',
+    'UX Design',
+    '3D printing',
+    'Hardware'
+
+]
