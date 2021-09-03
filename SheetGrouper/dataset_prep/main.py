ec64c030158d3f56b07f3683b68e29da99e4b88a
AlphaLaser <59497243+AlphaLaser@users.noreply.github.com>
2021-09-03 15:53:48 +0530

    Add files via upload

---
+++ SheetGrouper/dataset_prep/main.py
@@ -0,0 +1,14 @@
+def string_to_list_line (text) :
+
+    new_text = text.split('\n')
+
+    return new_text
+
+def string_to_list_space (text) :
+
+    new_text = text.split(' ')
+
+    return new_text
+
+
+
